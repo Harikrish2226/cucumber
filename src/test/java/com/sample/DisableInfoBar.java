70cd10d8b6992f90fbd286775b548c94a364d096
Harikrish2226 <sanuhari92@gmail.com>
2025-01-20 17:07:24 +0530

    Update DisableInfoBar.java

--- src/test/java/com/sample/DisableInfoBar.java
+++ src/test/java/com/sample/DisableInfoBar.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+//llkkk
 public class DisableInfoBar {
 	public static void main(String[] args) {
 		//close info bar in chrome browser
